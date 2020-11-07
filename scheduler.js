301d8273180db9aa951225dbdd1b3e0db38fd96e
ava brenneke <avabrenneke@gmail.com>
2020-11-07 10:12:09 -0800

    created user input text array and saved to local storage

--- scheduler.js
+++ scheduler.js
@@ -11,3 +11,15 @@ todaysDate();
 
 //Get Military Time
 var currentTime = moment().format("H");
+
+//Create Input:Text Array 
+$("button:submit").on("click", function () {
+    var userInput = $("input:text") //this is an array
+
+    //Save Input to Local Storage
+    for (var x = 0; x < userInput.length; x++) {
+        var id = $(userInput[x]).closest("tr").attr('id');
+        var input = $(userInput[x]).val();
+        localStorage.setItem(id, input);
+    }
+});
